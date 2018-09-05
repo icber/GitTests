31a292b4d4e37b4b74a30a77ecf7bbad3a94c01f
icber <yuanqiuleios@163.com>
2018-09-05 17:20:30 +0800

    Initial Commit

---
+++ GitTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTests
+//
+//  Created by yy on 2018/9/5.
+//  Copyright © 2018年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTests
+//
+//  Created by yy on 2018/9/5.
+//  Copyright © 2018年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
